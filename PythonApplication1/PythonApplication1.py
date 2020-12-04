b116a5bd9fc506b096b1c81f68f8c1f84abe5003
finkage <finkage@gmail.com>
2020-12-04 15:10:40 -0800

    persistant streaming is implemented. not satisfied with the speed at which each successive frame is grabbed. Possibly better to work with a solution that keeps this low "framerate" in mind rather than trying to fix the framerate.
    a potential solution however to the framerate should it be necessary is to split the process between multiple cores, one for capture, one for conversion to greyscale, and one to handle tesseract. asynchronization might become a problem but workable.

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,4 +1,5 @@
 import cv2
+from time import time
 import pytesseract as tess
 from class1 import WindowCapture
 
@@ -8,24 +9,36 @@ cong = r'--oem 1 --psm 6'
 
 #img = cv2.imread('test_log2.jpg')
 screen = WindowCapture('BlueStacks')
-img = screen.get_screenshot()
 
-#converting jpg into binary color so only text is visable
-img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-thresh, img = cv2.threshold(img, 110, 255, cv2.THRESH_BINARY)
+loopTime = time()
+while True:
+    #capture an image from the window
+    img = screen.get_screenshot()
 
-#debugging tool to see the recognized characters
-imgH, imgW = img.shape
-boxes = tess.image_to_boxes(img, lang = 'eng', config=cong)
-text = tess.image_to_string(img, lang = 'eng', config=cong)
+    #converting jpg into binary color so only text is visable
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    thresh, img = cv2.threshold(img, 110, 255, cv2.THRESH_BINARY)
+    text = tess.image_to_string(img, lang = 'eng', config=cong)
 
-for box in boxes.splitlines():
-    box = box.split(' ')
-    print(box)
-    x,y,w,h = int(box[1]), int(box[2]), int(box[3]), int(box[4])
-    cv2.rectangle(img, (x,imgH - y), (w,imgH - h), (0, 0, 255), 1)
+    '''
+    #debugging tool to see the recognized characters
+    imgH, imgW = img.shape
+    boxes = tess.image_to_boxes(img, lang = 'eng', config=cong)
+    for box in boxes.splitlines():
+        box = box.split(' ')
+        x,y,w,h = int(box[1]), int(box[2]), int(box[3]), int(box[4])
+        cv2.rectangle(img, (x,imgH - y), (w,imgH - h), (0, 0, 255), 1)
+    '''
 
-cv2.imshow('result', img)
-cv2.waitKey(0)
+    cv2.imshow('result', img)
+    print(text)
 
-print(text)
+    # debug the loop rate
+    print('FPS {}'.format(1 / (time() - loopTime)))
+    loopTime = time()
+
+    # press 'q' with the output window focused to exit.
+    # waits 1 ms every loop to process key presses
+    if cv2.waitKey(1) == ord('q'):
+        cv2.destroyAllWindows()
+        break
